5306eb04b0d5818cbbdeb66b8dd0b62fa409e402
Kush Bhagat <bhagat.kush.a@gmail.com>
2020-11-25 11:25:37 -0500

    First Example

--- server.js
+++ server.js
@@ -2,4 +2,24 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+const express = require('express');
+const fs = require('fs');
+var server = express();
 var jsonData = {count: 12, message: 'hey'};
+
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html', (err) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
+});
+
+server.get('/data', (req, res) => {
+    res.json(jsonData);
+});
+
+
+server.listen(3000, () => {
+    console.log('Listening on Port 3000')
+});
